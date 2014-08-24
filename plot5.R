cd83bdd27fd79881a9eecaf1fe233348c4b5d242
Adrian Hass <adrian.hass@gmail.com>
2014-08-24 22:58:29 +0200

    plot5

---
+++ plot5.R
@@ -0,0 +1,20 @@
+source("preliminaries.R")
+
+#prepare data
+motorVehicles <- SCC[grepl("veh",SCC$Short.Name,ignore.case=TRUE),"SCC"]
+
+plot5draw <- function() {
+        NEIbaltMV <- NEI[baltimore & NEI$SCC %in% motorVehicles,]
+        qplot(
+                year, Emissions,
+                data = NEIbaltMV,
+                geom=c("point","smooth"),method=lm
+        ) + ggtitle("Emissions of Motor Vehicles in Baltimore")
+}
+print(plot5draw())
+
+plot5 <- function() {
+        png("plot5.png")
+        print(plot5draw())
+        dev.off()
+}
